2704adc4cd190b0f7577f771d4fddb370bf11162
kleyson klaus <kleyson.zur44@gmail.com>
2023-08-16 18:14:13 -0500

    clases y sus metodos para poner privados y publicos

--- 01-TypeScript-intro/src/bases/03-classes.ts
+++ 01-TypeScript-intro/src/bases/03-classes.ts
@@ -8,14 +8,34 @@
 //     }
 // }
 export class Pokemon {
+    get imageUrl(): string {
+        return `https://pokemon.com/${this.id}/${this.name}.jpg`;
+    }
+
     constructor(
         public readonly id: number,
         public name: string,
+        // public imageUrl: string,
     ) { }
+
+
+    /// public por defecto
+    /// private solo dentro de la clase
+    public scream() {
+        console.log(`${this.name.toUpperCase()}!!!`);
+    }
+    speak() {
+        console.log(`${this.name} ${this.id}`);
+    }
+
 }
 
 
 export const charmander = new Pokemon(0, 'charmander');
 
 // charmander.id = 10; // no funciona por el readonly
-charmander.name = 'pikachu'; //funciona
+// charmander.name = 'pikachu'; //funciona
+
+console.log(charmander);
+charmander.scream();
+charmander.speak();
