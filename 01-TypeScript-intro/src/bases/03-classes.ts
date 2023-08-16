c42537d4233aa46f042af97114e3235237679d48
kleyson klaus <kleyson.zur44@gmail.com>
2023-08-16 18:30:42 -0500

    add paquete axios para get en http y como desestructurar data con {data}

--- 01-TypeScript-intro/src/bases/03-classes.ts
+++ 01-TypeScript-intro/src/bases/03-classes.ts
@@ -1,6 +1,11 @@
+import axios from "axios";
+import { isValid } from './01-types';
+
+
 // export class Pokemon {
 //     public id: number;
 //     public name: string;
+
 
 //     constructor(id: number, name: string) {
 //         this.id = id;
@@ -24,8 +29,17 @@ export class Pokemon {
     public scream() {
         console.log(`${this.name.toUpperCase()}!!!`);
     }
+
     speak() {
         console.log(`${this.name} ${this.id}`);
+    }
+
+    async getMoves() {
+        // const moves = 10;
+
+        const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/4');
+        console.log(data.moves);
+        return data;
     }
 
 }
@@ -36,6 +50,9 @@ export const charmander = new Pokemon(0, 'charmander');
 // charmander.id = 10; // no funciona por el readonly
 // charmander.name = 'pikachu'; //funciona
 
-console.log(charmander);
-charmander.scream();
-charmander.speak();
+// console.log(charmander);
+// charmander.scream();
+// charmander.speak();
+
+// console.log(charmander.getMoves() + 10);
+charmander.getMoves();
